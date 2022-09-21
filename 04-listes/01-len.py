8673b0c88ef03e0636584f74d8121e0e52e404ec
bcalou <bastien.calou@gmail.com>
2022-09-21 15:40:11 +0200

    chapter 4

---
+++ 04-listes/01-len.py
@@ -0,0 +1,55 @@
+# Il est possible de connaître la longueur d'une liste grâce à la fonction len().
+
+# Exemple : len([1, 2, 3]) -> 3
+
+# Utilisez cette fonction pour déterminer si il y a plus de membres chez les
+# Beatles ou chez Muse. 
+
+# Stockez le tableau le plus long dans une variable "biggest_band"
+
+beatles: list[str] = ["Paul McCarntey", "John Lennon", "Ringo Starr", "George Harisson"]
+muse: list[str] = ["Matthew James Bellamy", "Dominic James Howard", "Christopher Wolstenholme"]
+
+################################################################################
+
+################################################################################
+
+# Pensez à l'opérateur ternaire !
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print('\033[92m✓ OK' if biggest_band == beatles else '\033[91m❌KO')
