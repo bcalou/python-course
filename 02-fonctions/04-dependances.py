8f7043374cfb26b41b3e63e0716ac583653cd1bb
bcalou <bastien.calou@gmail.com>
2022-09-22 11:48:30 +0200

    close #4 rename get_square_volume to get_cube_volume

--- 02-fonctions/04-dependances.py
+++ 02-fonctions/04-dependances.py
@@ -6,14 +6,15 @@
 # Vous pouvez voir ci-dessous que la fonction get_square_description fait appel
 # à la fonction get_square_surface pour récupérer la surface du carré.
 
-# Créez une fonction get_square_volume prenant un paramètre "size".
+# Créez une fonction get_cube_volume prenant un paramètre "size".
 # Le volume étant la surface multipliée par la hauteur, utilisez la fonction
 # get_square_surface au sein de cette nouvelle fonction pour retourner le
 # volume.
 
 # Modifiez la fonction get_square_description pour qu'elle renvoit
-# "Voici un carré de 16cm2 de surface et de 48cm3 de volume", en remplaçant par
-# les valeurs appropriées et utilisant votre nouvelle fonction.
+# "Voici un carré de 16cm2 de surface et donnant un cube de 48cm3 de volume",
+# en remplaçant par les valeurs appropriées et utilisant votre nouvelle
+# fonction.
 
 def get_square_surface(size: float) -> float:
     """Get the surface of a square of the given size"""
@@ -55,4 +56,4 @@ print(get_square_description(4))
 
 
 
-print('\033[92m✓ OK\033[00m' if get_square_surface(3) == 9 and get_square_volume(3) == 27 and get_square_description(3) == "Voici un carré de 9cm2 de surface et de 27cm3 de volume" else '\033[91m❌KO\033[00m')
+print('\033[92m✓ OK\033[00m' if get_square_surface(3) == 9 and get_cube_volume(3) == 27 and get_square_description(3) == "Voici un carré de 9cm2 de surface et donnant un cube de 27cm3 de volume" else '\033[91m❌KO\033[00m')
