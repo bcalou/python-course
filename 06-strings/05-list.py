273185b62fcf86a52bf5e6f0b41fc24229d8623e
bcalou <bastien.calou@gmail.com>
2022-09-23 14:06:01 +0200

    improve 6/5

--- 06-strings/05-list.py
+++ 06-strings/05-list.py
@@ -2,7 +2,7 @@
 # possibles que sur une liste !
 
 # Créez une fonction is_valid_time() qui prend en paramètre un temps au format
-# "HH:mm" et qui retourne True si le format est valide et si l'heure existe.
+# "HHmm" et qui retourne True si le format est valide et si l'heure existe.
 
 ################################################################################
 
@@ -44,5 +44,5 @@
 
 
 
-print('\033[92m✓ OK\033[00m' if is_valid_time('23:59') and is_valid_time('00:00') and not is_valid_time('2:4') and not is_valid_time('25:00') and not is_valid_time('ab:cd') else '\033[91m❌KO\033[00m')
+print('\033[92m✓ OK\033[00m' if is_valid_time('2359') and is_valid_time('0000') and not is_valid_time('24') and not is_valid_time('2500') and not is_valid_time('abcd') else '\033[91m❌KO\033[00m')
 
