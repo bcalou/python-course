ae3315f45bb821c73d16f00d59a3d6b0918559ee
bcalou <bastien.calou@gmail.com>
2022-09-23 09:42:30 +0200

    chapter 5

---
+++ 05-boucles/00-for.py
@@ -0,0 +1,58 @@
+# Une boucle for permet de parcourir un tableau pour exécuter une opération ou
+# une vérification sur chaque valeur.
+
+# Exemple : for number in [7, 45, 65]
+# La variable "number" prendra successivement les valeurs 7, 45 et 65
+
+for number in [7, 45, 65]:
+    print(number)
+
+# Utilisez une boucle for pour calculer la moyenne des notes ci-dessous.
+# Stockez le résultat dans une variable "average"
+
+################################################################################
+notes: list[int] = [10, 15, 12, 8, 7]
+################################################################################
+
+# Note : si vous connaissez déjà python, vous savez peut-être qu'il existe des
+# méthodes plus simples pour calculer une moyenne... Mais chaque chose en son
+# temps.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print('\033[92m✓ OK\033[00m' if average == 10.4 else '\033[91m❌KO\033[00m')
