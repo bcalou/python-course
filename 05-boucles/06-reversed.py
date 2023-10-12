6569599122004b420817b0ca4c57e8caf492524b
bcalou <bastien.calou@gmail.com>
2023-10-12 14:24:46 +0200

    add chapter 5

---
+++ 05-boucles/06-reversed.py
@@ -0,0 +1,46 @@
+# Inverser l'ordre d'une boucle est un besoin récurrent.
+
+# Pour cela, il suffit d'utiliser la fonction reversed() sur le tableau ou le
+# range utilisé.
+
+for i in reversed(range(3)):
+    print(i)
+
+# Utilisez une boucle for inversée pour trouver le plus grand nombre dont le
+# carré est inférieur à 1000. Stockez le résultat dans une variable "result".
+
+# N'oubliez pas d'utiliser break lorsque vous avez trouvé, car ça ne sert à
+# rien de continuer (gâchis de performance).
+
+###############################################################################
+
+###############################################################################
+
+# Indice : de quel nombre partir ? n'importe lequel dont vous savez que son
+# carré est supérieur à 1000, par exemple 50 (car 50 * 50 = 2500).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print('\033[92m✓ OK\033[00m' if result == 31 else '\033[91m❌KO\033[00m')
---
+++ 05-boucles/09-boss.py
@@ -0,0 +1,57 @@
+# En mathématiques, la suite de Fibonacci est une suite d'entiers dans laquelle
+# chaque terme est la somme des deux termes qui le précèdent (Wikipédia).
+
+# En voici le début (on choisit arbitrairement 0 et 1 pour débuter) :
+# 0, 1, 1, 2, 3, 5, 8...
+
+# Écrivez une fonction find_in_fibonacci_suite()
+# Cette fonction prendra un tableau d'entiers.
+# La fonction doit retourner le premier entier de la listequi appartient à la
+# suite de Fibonacci (ou 0 si aucun ne correspond).
+
+# Exemple : [584, 987, 2] donne 987 (premier élément de la liste correspondant)
+
+###############################################################################
+
+###############################################################################
+
+find_in_fibonacci_suite([584, 987, 2])
+
+# D'abord, trouvez comment faire. Votre première version sera peut-être un peu
+# brute de décoffrage et peu optimisée. C'est alors la seconde partie du
+# travail qui commence : clarifiez et optimisez votre code.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print('\033[92m✓ OK\033[00m' if find_in_fibonacci_suite([1, 15, 75]) == 1 and find_in_fibonacci_suite([584, 987, 2]) == 987 and find_in_fibonacci_suite([144, 50, 89]) == 144 and find_in_fibonacci_suite([16, 87, 154]) == 0 else '\033[91m❌KO\033[00m')
